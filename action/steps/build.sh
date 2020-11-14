1ed461699db1c1ff33c1c57131aca768a6451650
Gabriel Defrance <gabriel.bottari.defrance@gmail.com>
2020-11-13 22:39:04 -0500

    fix: hotfix batchmode

--- action/steps/build.sh
+++ action/steps/build.sh
@@ -110,7 +110,6 @@ echo ""
 
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
   /opt/Unity/Editor/Unity \
-    -batchmode \
     -nographics \
     -logfile /dev/stdout \
     -quit \
